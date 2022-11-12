2d5ccdaaaef671ee76ce1e6a0eee89a85d977c41
Magomet <yuss.m91@gmail.com>
2022-11-12 11:09:32 +0600

    ConditionIfElse

---
+++ Exanple005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maga")
+{
+    Console.WriteLine("Salam, Maga");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
